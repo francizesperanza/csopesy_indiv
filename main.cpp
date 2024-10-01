4e8867bf1ef3d0ecc78b605fa31de62612a325b5
Franciz Emmanuelle Angelo Esperanza <franciz_esperanza@dlsu.edu.ph>
2024-10-02 02:21:52 +0800

    command is now complete

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <Windows.h>
 #include <iostream>
 #include <string>
+#include "nvidia-smi.h"
 
 
 int main()
 {
     std::string input;
-
     while (input != "exit") {
         std::cout << "Enter a command: ";
         std::getline(std::cin, input);
 
         if (input == "nvidia-smi") {
-            std::cout << "nvidia-smi command detected." << std::endl;
+            print_console_layout();
         }
         else if (input == "exit") {
             std::cout << "Program exiting..." << std::endl;
